970a70616241f5560a27715ba1ac062e2e7d02b3
Benjamin Coban <Benjamin.Coban@gmail.com>
2025-08-31 23:50:57 +0200

    loop labelling
    the main loop continues to increment "count" until the user is pressing a button or u32::MAX is reached
    
    usage of std::io/sync/thread for signalling

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,29 @@
-// Unsigned 8-bit integer 255 gets incremented. Lets panic!
-
-fn panic_u8_runtime() {
-    let x: u8 = 1;
-    for i in 0..=255 {
-        let _y = x + i; // This will panic when i is 255
-    }
-}
+use std::io::{self, Read};
+use std::sync::mpsc;
+use std::thread;
 
 fn main() {
-    println!("This is a overflow test");
-}
+    // Create a channel to signal when user presses a key
+    let (tx, rx) = mpsc::channel();
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+    // Spawn a thread to listen for input
+    thread::spawn(move || {
+        let mut buf = [0u8; 1];
+        let stdin = io::stdin();
+        let mut handle = stdin.lock();
 
-    #[test]
-    #[should_panic(expected = "attempt to add with overflow")]
-    fn test_panic_u8_runtime() {
-        panic_u8_runtime();
+        // Wait until user presses any key + ENTER
+        handle.read(&mut buf).unwrap();
+        tx.send(()).unwrap();
+    });
+    let mut count: u32 = 0;
+    // Main loop keeps running until user presses a key
+    'testloop: loop {
+        count += 1;
+        // Break if input thread sent a message
+        if rx.try_recv().is_ok() || count == u32::MAX{
+            println!("User pressed a key, breaking loop! Count: {}", count);
+            break 'testloop;
+        }
     }
 }
